7b7b30a7404ff52fce4bc00f05dcb241de24e1ee
Wonwoo Choi <chwo9843@gmail.com>
2022-01-11 16:33:52 +0900

    Add engine options

--- src/bin/tweet-broadcast.rs
+++ src/bin/tweet-broadcast.rs
@@ -1,23 +1,46 @@
+use std::collections::HashSet;
+
+use clap::Parser;
 use tokio::signal::unix as unix_signal;
 
 use tweet_broadcast::{tweet::TwitterClient, Router};
 
+#[derive(Debug, PartialEq, Eq, Hash, strum::EnumString, strum::Display)]
+#[strum(serialize_all = "snake_case")]
+enum Engine {
+    FilteredStream,
+    List,
+}
+
+#[derive(Debug, Parser)]
+#[clap(version)]
+struct Args {
+    #[clap(short, long, env = "TWITTER_CACHE", default_value = "./.tweets")]
+    cache: std::path::PathBuf,
+    #[clap(short, long = "engine")]
+    engines: Vec<Engine>,
+}
+
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    let Args {
+        cache: cache_dir,
+        mut engines,
+    } = Args::parse();
 
-    let token = std::env::var("TWITTER_APP_TOKEN").expect("TWITTER_APP_TOKEN not found or invalid");
-    let cache_dir = std::env::var_os("TWITTER_CACHE")
-        .or_else(|| {
-            let path = std::env::current_dir().ok()?;
-            Some(path.join(".tweets").into_os_string())
-        })
-        .and_then(|path| std::fs::create_dir_all(&path).ok().map(|_| path))
-        .expect("Invalid cache directory");
-    let cache_dir = std::path::PathBuf::from(cache_dir);
+    if engines.len() == 0 {
+        engines.push(Engine::FilteredStream);
+        engines.push(Engine::List);
+    }
+    let engines = engines.into_iter().collect::<HashSet<_>>();
+
+    std::fs::create_dir_all(&cache_dir).expect("Invalid cache directory");
     std::fs::create_dir_all(cache_dir.join("meta")).unwrap();
     std::fs::create_dir_all(cache_dir.join("images")).unwrap();
 
+    let token = std::env::var("TWITTER_APP_TOKEN").expect("TWITTER_APP_TOKEN not found or invalid");
+
+    env_logger::init();
     let _sentry = sentry::init((
         std::env::var_os("SENTRY_DSN"),
         sentry::ClientOptions {
@@ -41,21 +64,27 @@ async fn main() {
 
     let local_set = tokio::task::LocalSet::new();
 
-    let stream_handle = {
+    let stream_handle = if engines.contains(&Engine::FilteredStream) {
+        log::info!("Enabling engine {}", Engine::FilteredStream);
         let client = client.clone();
-        local_set.spawn_local(async move {
+        Some(local_set.spawn_local(async move {
             let mut router = Router::new(128 * 1024 * 1024).expect("Failed to load router");
             loop {
                 client.run_stream(&mut router).await.err();
             }
-        })
+        }))
+    } else {
+        None
     };
-    let list_handle = {
+    let list_handle = if engines.contains(&Engine::List) {
+        log::info!("Enabling engine {}", Engine::List);
         let client = client.clone();
-        tokio::spawn(async move {
+        Some(tokio::spawn(async move {
             let e = client.run_list_loop().await.unwrap_err();
             log::error!("List error: {}", e);
-        })
+        }))
+    } else {
+        None
     };
 
     let sig_handle = tokio::spawn(async move {
@@ -67,10 +96,18 @@ async fn main() {
         futures_util::pin_mut!(sigquit);
 
         futures_util::future::select_all([sigterm, sigint, sigquit]).await;
-        stream_handle.abort();
-        list_handle.abort();
-        stream_handle.await.err();
-        list_handle.await.err();
+        if let Some(stream_handle) = &stream_handle {
+            stream_handle.abort();
+        }
+        if let Some(list_handle) = &list_handle {
+            list_handle.abort();
+        }
+        if let Some(stream_handle) = stream_handle {
+            stream_handle.await.err();
+        }
+        if let Some(list_handle) = list_handle {
+            list_handle.await.err();
+        }
     });
 
     local_set.await;
